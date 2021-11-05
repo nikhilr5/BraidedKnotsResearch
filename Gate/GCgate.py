90381509199f3d800c80b876ff4cb7d47c503aaf
王小涵 <wangxiaohan@wirelessprvnat-172-16-201-44.near.illinois.edu>
2021-11-05 12:50:17 -0500

    new gate class for the gc coloring stuff

---
+++ Gate/GCgate.py
@@ -0,0 +1,46 @@
+class GCgate:
+    """
+    a set of bijection function (gates) for the GC coloring stuff
+    extremely skeletal for now
+    """
+    def __init__(self, function, x, name):
+        """Initiates a new gate.
+            Arguements: 
+                - function: the bijection function
+                - x: input a list
+                - name: just a name :)
+        """
+        self.input = x
+        self.name = name
+        self.gate = function
+        self.output = list()
+
+        if not self.check_input():
+            raise ValueError('Invalid input')
+        if not self.check_function():
+            raise ValueError('Invalid gate function')
+
+        # self.output = function(x)
+        
+    
+    def check_input(self):
+        """
+        helper function no.1
+        check if input is valid, should check length & alphabet
+        should be a list of length 2, each element in the list should be a quadruple (?)
+        return True if input is valid, False otherwise
+        """
+        return False
+
+    def check_function(self):
+        """
+        helper function no.2
+        check if the gate function is valid (how to identify?) 
+        return True if function is valid, False otherwise
+        """
+        return False
+
+    ## a dictionary for the elements in C <- conjugacy class
+    ## propably later, need to look at sage data structure first D:
+    c_class = {
+    }
