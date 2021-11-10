9eebab899a21afb8c1067d505fa94d2d4e2dc29d
rocjay1 <jasonroc19@gmail.com>
2021-11-10 09:38:49 -0600

    add gate function check to BBGate/TBGate classes

--- Gate/Gate.py
+++ Gate/Gate.py
@@ -53,8 +53,12 @@ print(g.evaluate(x))
 class BBGate(Gate):
 
     def __init__(self, name, function, k):
-        super(BBGate, self).__init__(name, function, {0,1}, k, 1)
+        # Check if function is one of the valid BB gates
+        if function not in [{(0,0): 0, (1,0): 0, (0,1): 0, (1,1): 1}, {(0,0): 0, (1,0): 1, (0,1): 1, (1,1): 1}, {0: 1, 1: 0}]:
+            raise ValueError("Incorrect gate function")
 
+        super(BBGate, self).__init__(name, function, {0,1}, k, 1)
+    
 # Binary boolean 'and' gate class, a child of BBGate representing an 'and' gate
 class BBAndGate(BBGate):
 
@@ -66,37 +70,43 @@ class BBOrGate(BBGate):
     def __init__(self):
         super(BBOrGate, self).__init__('Or', {(0,0): 0, (1,0): 1, (0,1): 1, (1,1): 1}, 2)
 
-class BBOrGate(BBGate):
-
-    def __init__(self):
-        super(BBOrGate, self).__init__('Or', {(0,0): 0, (1,0): 1, (0,1): 1, (1,1): 1}, 2)
-
 class BBNotGate(BBGate):
 
     def __init__(self):
         super(BBNotGate, self).__init__('Not', {0: 1, 1: 0}, 1)
 
-# g = BBAndGate()
-# x = (1,0)
-# print(g.evaluate(x))
-# y = (1,1)
-# print(g.evaluate(y))
+g = BBAndGate()
+x = (1,0)
+print(g.evaluate(x))
+y = (1,1)
+print(g.evaluate(y))
 
-# g = BBOrGate()
-# x = (1,0)
-# print(g.evaluate(x))
-# y = (1,1)
-# print(g.evaluate(y))
+g = BBOrGate()
+x = (1,0)
+print(g.evaluate(x))
+y = (1,1)
+print(g.evaluate(y))
 
-# g = BBNotGate()
-# x = 0
-# print(g.evaluate(x))
+g = BBNotGate()
+x = 0
+print(g.evaluate(x))
 
 # Same stuff for ternary boolean gates, where alphabet is {0,1},
 # and functions are on triples (and return triples)
 class TBGate(Gate):
 
     def __init__(self, name, function):
+        l = []
+        for x in {0,1}:
+            for y in {0,1}:
+                for z in {0,1}:
+                    l.append((x,y,z))
+        a = {x: (((x[0] and x[1]) + x[2]) % 2, x[0], x[1]) for x in l}
+        o = {x: (((x[0] or x[1]) + x[2]) % 2, x[0], x[1]) for x in l}
+        n = {x: (1 - x[0], x[1], x[2]) for x in l}
+        if function not in [a, o, n]:
+            raise ValueError("Incorrect gate function")
+
         super(TBGate, self).__init__(name, function, {0,1}, 3, 3)
 
 class TBAndGate(TBGate):
@@ -108,10 +118,15 @@ class TBAndGate(TBGate):
                 for z in {0,1}:
                     l.append((x,y,z))
         f = {x: (((x[0] and x[1]) + x[2]) % 2, x[0], x[1]) for x in l}
+
         super(TBAndGate, self).__init__('And', f)
 
 g = TBAndGate()
 x = (1, 0, 0)
 print(g.evaluate(x))
 
-    
+# Example of bad BB gate attempt, get value error
+
+h = BBGate('bad', {0: 1}, 1)
+x = 0
+print(g.evaluate(x))
