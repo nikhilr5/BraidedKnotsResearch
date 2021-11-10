8d3454672cca75a17e21ba5e0ebd31df334100d8
Nikhil Richard <42984048+nikhilr5@users.noreply.github.com>
2021-11-09 18:46:08 -0600

    Gate class revision

--- Gate/Gate.py
+++ Gate/Gate.py
@@ -1,35 +1,52 @@
-from abc import abstractmethod
-
 class Gate:
 
-    def __init__(self, name):
+    def __init__(self, name, function, alphabet, k, l):
         self.name = name
+        self.function = function
+        self.alphabet = alphabet
+        self.k = k
+        self.l = l
 
     def __str__(self):
         """
         Returns:
-              Inputs and Outputs of gate.
+              Gate information
         """
-        return f' Input of Binary gate: {self.input} \n Ouput of binary gate: {self.output}'
+        return f' Name: {self.name} \n Function: {self.function} \n Alphabet: {self.alphabet} \n k: {self.k} \n l: {self.l}'
 
-    @abstractmethod
-    def and_gate(self):
-        pass
-
-    @abstractmethod
-    def or_gate(self):
-        pass
-
-    @abstractmethod
-    def not_gate(self):
-        pass
 
     def getName(self):
         return self.name
 
-    def getInput(self):
-        return self.input
+    def getFunction(self):
+        return self.function
+
+    def getAlphabet(self):
+        return self.alphabet
     
-    def getOutput(self):
-        return self.output
+    def getK(self):
+        return self.k
+    
+    def getL(self):
+        return self.l
+
+    def evaluate(self, x):
+        return self.function[(x)]
+
+    
+orGate =	{
+  (0,0): 0,
+  (0,1): 1,
+  (1,0): 2,
+  (1,1): 3
+}
+
+g = Gate(name="g", function=orGate, alphabet={0,1}, k=2, l=1)
+
+x = (1,0)
+print(g.evaluate(x))
+
+        
+
+
     
